7f1f4a1bc61ac64d76c7845f05afceb629b4ac86
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-11-19 08:39:01 -0600

    Add inmeasure analytics script to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { HeaderNav } from "~/components/layout/HeaderNav";
 import { Footer } from "~/components/layout/Footer";
-import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 import type { Metadata } from "next";
 
@@ -12,6 +12,17 @@ export const metadata: Metadata = {
   description: "Tommy Wells.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
+
+const inMeasureScript = `(function () {
+  if (!document.getElementById("im-script")) {
+    const element = document.createElement("script");
+    element.type = "text/javascript";
+    element.async = true;
+    element.src = "https://www.inmeasure.com/api/v1/script/da01dd72-9d43-4717-9230-df4b28576796";
+    element.id = "im-script";
+    document.head.appendChild(element);
+  }
+})();`
 
 export default function RootLayout({
   children,
@@ -25,7 +36,13 @@ export default function RootLayout({
         <HeaderNav />
         <main className="w-full max-w-[700px] my-32">{children}</main>
         <Footer />
-        <Analytics />
+        <Script
+          id="inmeasure-script"
+          dangerouslySetInnerHTML={{
+            __html: inMeasureScript,
+          }}
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
