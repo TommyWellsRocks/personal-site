3af6f59face5aaea54761a37a5a158b5f7038089
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-12-10 15:16:51 -0600

    Update projects and export projects list to own file.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,35 +10,7 @@ import { ArrowUpRight } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 import { InfoSection } from "../../components/projects/InfoSection";
 
-import TemplePreview from "public/TemplePreview.png";
-import MLMPreview from "public/MLMPreview.png";
-
-const projects = [
-  {
-    id: "temple",
-    name: "Temple",
-    img: TemplePreview,
-    githubLink: "",
-    shortSummary: "Fitness app to build and record workouts.",
-    description:
-      "Fitness app for recording and creating workouts. Build your own workout programs with days with any of our over 4000 exercises (or create your own). Record sets, view history, keep notes, and more.",
-    tools: ["React", "Next.js", "PostgreSQL", "Vercel", "Zustand"],
-    link: "https://trytemple.com",
-    video: "/TempleVideo.mp4",
-  },
-  {
-    id: "mlm",
-    name: "Marvelous Light Media",
-    img: MLMPreview,
-    githubLink: "",
-    shortSummary: "Wedding videography website.",
-    description:
-      "Website for Haley Harris at Marvelous Light Media, a wedding videography company here in Nashville.",
-    tools: ["React", "Next.js", "Netlify", "CDN"],
-    link: "https://marvelouslightmedia.com",
-    video: "",
-  },
-];
+import { projects } from "~/data/projects";
 
 export type Project = (typeof projects)[0];
 
@@ -62,7 +34,7 @@ export default function Projects() {
           <Sheet key={index}>
             <SheetTrigger>
               <div
-                className="relative flex min-h-[350px] text-start sm:aspect-video scroll-mt-[100px]"
+                className="relative flex min-h-[350px] scroll-mt-[100px] text-start sm:aspect-video"
                 id={project.id}
               >
                 {project.video ? (
@@ -79,13 +51,13 @@ export default function Projects() {
                   <ProjectImage project={project} />
                 )}
 
-                <div className="z-10 flex w-full flex-col items-start justify-end gap-y-2 rounded-lg border-4 border-slate-700 bg-gradient-to-b from-transparent to-slate-900 px-2 py-4 sm:p-10 hover:border-slate-600">
+                <div className="z-10 flex w-full flex-col items-start justify-end gap-y-2 rounded-lg border-4 border-slate-700 bg-gradient-to-b from-transparent to-slate-900 px-2 py-4 hover:border-slate-600 sm:p-10">
                   <Link
                     target="_blank"
                     href={project.link}
                     className="flex text-3xl font-bold text-slate-300 hover:text-slate-100"
                   >
-                    {project.name} <ArrowUpRight width={20} />
+                    {project.name} <ArrowUpRight size={20} />
                   </Link>
                   <span className="font-light">{project.shortSummary}</span>
                   <ToolList tools={project.tools} />
---
+++ src/data/projects.ts
@@ -0,0 +1,74 @@
+import TemplePreview from "public/TemplePreview.png";
+import MLMPreview from "public/MLMPreview.png";
+import ThisPreview from "public/ThisPreview.jpg";
+import InMeasurePreview from "public/InMeasurePreview.png";
+
+export const projects = [
+  {
+    id: "temple",
+    name: "Temple",
+    img: TemplePreview,
+    githubLink: "",
+    shortSummary: "Fitness app to build and record workouts.",
+    description:
+      "Fitness app for recording and creating workouts. Customize workouts, record sets, view history, keep notes, and more.",
+    tools: ["React", "Next.js", "PostgreSQL", "Vercel", "Zustand", "Node.js"],
+    link: "https://www.trytemple.com",
+    video: "/TempleVideo.mp4",
+  },
+  {
+    id: "mlm",
+    name: "Marvelous Light Media",
+    img: MLMPreview,
+    githubLink: "",
+    shortSummary: "Wedding videography website.",
+    description:
+      "Website for Marvelous Light Media, a wedding videography company.",
+    tools: ["React", "Next.js", "Netlify", "CDN"],
+    link: "https://www.marvelouslightmedia.com",
+    video: "",
+  },
+  {
+    id: "this",
+    name: "TommyWells.me",
+    img: ThisPreview,
+    githubLink: "",
+    shortSummary: "This wonderful site.",
+    description: "A portfolio / resume site to show off some of my work.",
+    tools: ["React", "Next.js"],
+    link: "https://www.tommywells.me",
+    video: "",
+  },
+  {
+    id: "inmeasure",
+    name: "InMeasure",
+    img: InMeasurePreview,
+    githubLink: "",
+    shortSummary: "Website analytics tool.",
+    description:
+      "Web/Product analytics tool to track and monitor real-time site analytics data.",
+    tools: [
+      "React",
+      "Next.js",
+      "Vercel",
+      "REST APIs",
+      "Zustand",
+      "PostgreSQL",
+      "Node.js",
+    ],
+    link: "https://www.inmeasure.com",
+    video: "/InMeasureVideo.mp4",
+  },
+  // {
+  //   id: "CMPD12",
+  //   name: "CMPD12 Capital",
+  //   img: MLMPreview,
+  //   githubLink: "",
+  //   shortSummary: "Financial management website.",
+  //   description:
+  //     "Website for CMPD12 Capital, a financial advisory and asset management company.",
+  //   tools: ["React", "Next.js", "Vercel", "PostgreSQL", "AWS S3", "Node.js"],
+  //   link: "https://www.cmpd12.com",
+  //   video: "",
+  // },
+];
