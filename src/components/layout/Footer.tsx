0f8e83dd99779323585138ced1d9f5805e0c5fbd
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2025-02-24 10:09:19 -0600

    Connect with me links and footer adjustment

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,11 @@
 import { Connect } from "~/components/layout/Connect";
 
 export function Footer() {
+  const year = new Date().getFullYear()
   return (
-    <footer className="fixed bottom-2 right-2 z-50 flex flex-col items-center gap-y-2 p-5 text-slate-500 backdrop-blur-md">
+    <footer className="flex flex-col items-center gap-y-4 p-5 text-slate-500 backdrop-blur-md">
       <Connect />
+      <span className="text-xs">© {year} Tommy Wells. All rights reserved.</span>
     </footer>
   );
 }
