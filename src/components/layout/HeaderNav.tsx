d9d9cf99ca8b8a0902be4d40a380974344ae146b
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2025-02-24 10:08:53 -0600

    Remove too much special text

--- src/components/layout/HeaderNav.tsx
+++ src/components/layout/HeaderNav.tsx
@@ -10,10 +10,7 @@ export function HeaderNav() {
   return (
     <header className="fixed left-0 right-0 top-0 z-50 mx-auto max-w-[1300px] px-4 backdrop-blur-md sm:px-10">
       <div className="flex items-center justify-between py-4 text-slate-500">
-        <Link
-          className="hover-fancy-text-shadow text-4xl font-bold hover:text-slate-300"
-          href="/"
-        >
+        <Link className="text-4xl font-bold hover:text-slate-300" href="/">
           TW
         </Link>
         <nav className="hidden gap-x-4 text-lg font-bold min-[320px]:flex">
@@ -21,7 +18,7 @@ export function HeaderNav() {
             <Link
               key={index}
               href={item.link}
-              className="hover-fancy-text-shadow text-slate-300 transition duration-300 hover:text-slate-50"
+              className="text-slate-300 transition duration-300 hover:text-slate-50"
             >
               {item.name}
             </Link>
