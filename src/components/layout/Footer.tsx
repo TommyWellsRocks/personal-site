2e660d2a772ff796dc8c2cf6a8a79d722b17dfb0
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2025-02-24 10:16:28 -0600

    Fix footer position

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Connect } from "~/components/layout/Connect";
 export function Footer() {
   const year = new Date().getFullYear()
   return (
-    <footer className="flex flex-col items-center gap-y-4 p-5 text-slate-500 backdrop-blur-md">
+    <footer className="fixed bottom-0 w-full flex flex-col items-center z-50 gap-y-4 p-5 text-slate-500 backdrop-blur-md">
       <Connect />
       <span className="text-xs">© {year} Tommy Wells. All rights reserved.</span>
     </footer>
